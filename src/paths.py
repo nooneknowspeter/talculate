cedc3b807cfbb9a57216a46de26520378c51df99
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-07-16 01:45:48 -0400

    feat: add config folder paths based on os

--- src/paths.py
+++ src/paths.py
@@ -7,5 +7,7 @@ cwd = os.getcwd()
 xdg_config_home = xdg.xdg_config_home()
 
 if sys.platform == "win32":
+    talc_config_folder_path = os.path.join("$env:LOCALAPPDATA\\talculate")
 else:
+    talc_config_folder_path = os.path.join(xdg_config_home, "talculate")
 
