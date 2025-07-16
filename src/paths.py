0a76b3de3a4af9ce6860f1539b839b9463884fc0
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-07-16 01:47:51 -0400

    feat: create default config file in config dir

--- src/paths.py
+++ src/paths.py
@@ -11,6 +11,23 @@ if sys.platform == "win32":
 else:
     talc_config_folder_path = os.path.join(xdg_config_home, "talculate")
 
+config_file_path = os.path.join(talc_config_folder_path, "config.yaml")
 if not os.path.exists(talc_config_folder_path):
     os.mkdir(talc_config_folder_path)
+
+if not os.path.exists(config_file_path):
+    default_config_file = """# default config
+bindings:
+  quit: "q"
+  paste: "v"
+  copy_selected: "c"
+  copy_line_var: "l"
+  toggle_focus: "tab"
+  toggle_hex: "ctrl+x"
+  toggle_binary: "ctrl+b"
+theme:
+"""
+
+    with open(config_file_path, "w") as file:
+        file.write(default_config_file)
 
