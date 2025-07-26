fce368441b57eb27f48fb714f3468f54d6304437
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-07-26 11:34:54 -0400

    chore(nix): wrap devShell in flake-utils

--- flake.nix
+++ flake.nix
@@ -6,22 +6,25 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs@{ ... }:
-    let
-      supportedSystems =
-        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f:
-        inputs.nixpkgs.lib.genAttrs supportedSystems
-          (system: f { pkgs = import inputs.nixpkgs { inherit system; }; });
-    in
-    {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [ poetry python3 gnumake ];
-          shellHook = ''
-            export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH"
-          '';
+  outputs = { self, nixpkgs, flake-utils, }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells = {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              black
+              nixfmt-rfc-style
+
+              gnumake
+              poetry
+              python3
+              treefmt
+            ];
+            shellHook = ''
+              export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH"
+            '';
+          };
         };
       });
-    };
 }
